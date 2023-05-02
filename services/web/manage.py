387b843a816a98c4ed8138444e69f29dcf8a406d
jreznick <jreznick@gmail.com>
2023-05-02 00:03:29 +0000

    rm scaffold from manage.py

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,5 +1,5 @@
 from flask.cli import FlaskGroup
-from project import app, db, User
+from project import app, db
 
 cli = FlaskGroup(app)
 
@@ -10,10 +10,12 @@ def create_db():
     db.create_all()
     db.session.commit()
 
+'''
 @cli.command("seed_db")
 def seed_db():
     db.session.add(User(email="michael@mherman.org"))
     db.session.commit()
+'''
 
 
 if __name__ == "__main__":
