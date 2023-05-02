620c4d9a32fee051691ad2f2c2a8d374c2a6753f
jreznick <jreznick@gmail.com>
2023-05-02 13:23:53 +0000

    engine

---
+++ services/web/project/engine.py
@@ -0,0 +1,123 @@
+from sqlalchemy import and_, or_
+from time import time
+
+from .app import app
+from .matching import (
+    compare_nameday_equal, 
+    compare_ssn_equal, 
+    wrap_address_check, 
+    wrap_name_check
+)
+from .model import db, Demographic
+
+
+def parse_result(metrics: dict) -> bool:
+    threshold = metrics.get('threshold')
+    score = metrics.get('score')
+    if score >= threshold:
+        return True
+    else:
+        return False
+
+
+def toy_fine_matching(record_a, record_b):
+    stride = 0.3
+    match_score = 0
+    threshold = 0.5
+    if record_a.get(postal_code) == record_b.get('postal_code'):
+        match_score += stride
+    if record_a.get(name_day) == record_b.get('name_day'):
+        match_score += stride
+    if record_a.get(family_name) == record_b.get('family_name'):
+        match_score += stride
+    toy_fine_match = {
+        "record_a_id": record_a.get('record_id'),
+        "record_b_id": record_b.get('record_id'),
+        "score": match_score,
+        "threshold": threshold
+    }
+    toy_fine_match["match"] = parse_result(toy_fine_match)
+
+    return toy_fine_match
+
+
+def fine_matching(record_a: dict, record_b: dict):
+    """
+    :param record_a: the new demographics record 
+    :param record_b: the coarse match record against which the new record is tested
+    """
+    start = time()
+    # ToDo: score and threshold
+    fine_match = {"address_matching": wrap_address_check(record_a, record_b),
+                  "model_score": None,
+                  "name_matching": wrap_name_check(record_a, record_b),
+                  "name_day_matching": compare_nameday_equal(
+                    record_a.get("name_day", None), record_b.get("name_day", None)
+                    ),
+                  "ssn_matching": compare_ssn_equal(
+                    record_a.get("social_security_number", None), 
+                    record_b.get("social_security_number", None)
+                    ),
+                  "score": 0,
+                  "threshold": 0}
+    fine_match["match"] = parse_result(fine_match)
+    end = time()
+    fine_match["exec_time"] = f"{end - start:.8f}"
+
+    return fine_match
+
+
+def toy_coarse_matching(demographic_record) -> list:
+    with app.app_context():
+        record_id = demographic_record.get('record_id', None)
+        postal_code = demographic_record.get('postal_code')
+        name_day = demographic_record.get('name_day')
+        family_name = demographic_record.get('family_name')
+        coarse_results = []
+        source_table = Demographic
+        query = Demographic.query
+        query = query.filter(or_(
+            source_table.__table__.c['postal_code'] == postal_code,
+            source_table.__table__.c['name_day'] == name_day,
+            source_table.__table__.c['family_name'] == family_name))\
+            .filter(and_(source_table.__table__.c['record_id'] != record_id))
+        for row in query.all():
+            coarse_results.append(row)
+
+    return coarse_results
+
+
+def coarse_matching(demographics_record) -> list:
+    """
+    :param demographics_record: The input demographics record
+    """
+    #ToDo: implement data-aware coarse matching technique
+    #This bypass leaves coarse matching in guaranteed toy mode for now
+    coarse_results = toy_coarse_matching(demographics_record)
+
+    return coarse_results
+
+# implement blocking & filtering technique here with coarse, fine matching modes
+MODES = {
+    "toy": (toy_coarse_matching, toy_fine_matching),
+    "prod": (coarse_matching, fine_matching)
+}
+MODE = "toy"
+
+
+def compute_all_matches(demographic_record) -> (list, str):
+    """
+    :param demographic_record: The input demographics record
+    """
+    with app.app_context():
+        db.create_all()
+        coarse_matcher, fine_matcher = MODES[MODE]
+        start = time()
+        computed_matches = []
+        for coarse_match in coarse_matcher(demographic_record):
+            if demographic_record.get('record_id') != coarse_match.get('record_id'):
+                computed_matches.append(fine_matcher(demographic_record, coarse_match))
+        end = time()
+        exec_time = f"{end - start:.8f}"
+
+    return computed_matches, exec_time
---
+++ test/test_engine.py
@@ -0,0 +1,66 @@
+from services.web.project import timeit
+from services.web.project.data_utils import demographics_record
+from services.web.project.engine import (
+    compute_all_matches,
+    coarse_matching,
+    fine_matching,
+    parse_result,
+)
+
+
+@timeit
+def test_compute_all_matches():
+    key = "test_compute_all_matches"
+    input_fixture = demographics_record(key)
+    expected_result = {
+            'address_matching': None,
+            'model_score': None,
+            'name_matching': None,
+            'name_day_matching': None,
+            'ssn_matching': None
+    }
+    actual_results, _ = compute_all_matches(input_fixture)
+    for actual_result in actual_results:
+        assert actual_result.keys() == expected_result.keys()
+
+
+@timeit
+def test_coarse_matching():
+    key = "test_coarse_matching"
+    input_fixture = demographics_record(key)
+    expected_result = []
+    actual_result = coarse_matching(input_fixture)
+    assert expected_result == actual_result
+
+
+@timeit
+def test_fine_matching():
+    key = "test_fine_matching"
+    input_fixture_1 = demographics_record(f'{key}_1')
+    input_fixture_2 = demographics_record(f'{key}_2')
+    expected_result = {
+        'address_matching': None,
+        'match': None,
+        'exec_time': None,
+        'model_score': None,
+        'name_matching': None,
+        'name_day_matching': None,
+        'ssn_matching': None,
+        'score': 0,
+        'threshold': 0
+    }
+    actual_result = fine_matching(input_fixture_1, input_fixture_2)
+    assert sorted(expected_result.keys()) == sorted(actual_result.keys())
+
+
+@timeit
+def test_parse_result():
+    input_fixture = {'score': 0.6, 'threshold': 0.5}
+    expected_result = True
+    actual_result = parse_result(input_fixture)
+    assert expected_result == actual_result
+
+    input_fixture = {'score': 0.4, 'threshold': 0.5}
+    expected_result = False
+    actual_result = parse_result(input_fixture)
+    assert expected_result == actual_result
